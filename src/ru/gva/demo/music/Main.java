5a1fcd32ecdb499ca797677f2612cbde78e3fdc4
vadim <vadim.gavrikov.1998@gmail.com>
2017-12-19 15:15:41 +0300

    изменен способ хранение регулярного вырожения и url адреса сайта

--- src/ru/gva/demo/music/Main.java
+++ src/ru/gva/demo/music/Main.java
@@ -4,34 +4,51 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ *
+ */
 public class Main {
 
     private static final String IN_FILE_TXT = "src/ru/gva/demo/music/InFile.txt";
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        String[] url = new String[2];
-        String[] pattern = {"\\s*(?<=data-url\\s?=\\s?\")[^>]*\\/*(?=\")", "\\s*(?<=audio src=\")[^>]*\\.mp3", "\\s*(?<=href=\")[^>]*\\.mp3"};
+        Map<String, String> pathAndURL = new HashMap<>();
+
+        String str;
+        String[] splitStr;
 
         try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT))) {
 
-            for (int i = 0; i < url.length; i++) {
+            while ((str = inFile.readLine()) != null) {
+                splitStr = str.split(" ");
+//                for (int i = 0; i <splitStr.length ; i++) {
+//                 System.out.println(splitStr[0]);
+//                System.out.println(splitStr[1]);
+//                }
+                pathAndURL.put(splitStr[0], splitStr[1]);
 
-                url[i] = inFile.readLine();
             }
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+        String[] strings = pathAndURL.keySet().toArray(new String[0]);
+        for (int i = 0; i <strings.length ; i++) {
+            System.out.println(pathAndURL.get(strings[i]));
+        }
+//        pathAndURL.put("https://zvonko.me/", "\\s*(?<=data-url\\s?=\\s?\")[^>]*\\/*(?=\")");
+//        pathAndURL.put("https://mp3up.me/", "\\s*(?<=audio\\s?src=\")[^>]*\\.mp3");
+        //ParrserHtmlSafeResultInFileTXT.parrseout(pathAndURL);
 
-        ParrserHtmlSafeResultInFileTXT.parrseout(url, pattern);
+
+     //   MusicDownload music1 = new MusicDownload();
 
 
-        MusicDownload music1 = new MusicDownload(0);
-        // MusicDownload music2 = new MusicDownload(1);
-
-        music1.start();
-        // music2.start();
+       // music1.start();
     }
+
 
 }

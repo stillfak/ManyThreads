a893300b447d47686ca5857674b705f6ded3d418
vadim <vadim.gavrikov.1998@gmail.com>
2017-12-19 12:10:41 +0300

    Добавлен пакет задачи "качаем картинки" с использованием класса DownloadUsingNIO из пакета music

---
+++ src/ru/gva/demo/downloadJPG/Main.java
@@ -0,0 +1,15 @@
+package ru.gva.demo.downloadJPG;
+
+import ru.gva.demo.music.DownloadUsingNIO;
+
+public class Main {
+    private static final String PATH_TO_JPEG ="src/ru/gva/demo/downloadJPG/";
+
+    public static void main(String[] args) {
+
+        String url = "https://cdn.pixabay.com/photo/2017/05/15/17/43/cup-2315554_960_720.jpg";
+
+        new DownloadUsingNIO(url,PATH_TO_JPEG + "1"+".jpg").start();
+
+    }
+}

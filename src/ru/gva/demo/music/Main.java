69aa2c823157851191c2e150043e7b81e15cce93
vadim <vadim.gavrikov.1998@gmail.com>
2017-12-23 08:39:32 +0300

    исправленна ошибка,  с передачей в ассоциативный массив значений и добавленны комментарии к классу

--- src/ru/gva/demo/music/Main.java
+++ src/ru/gva/demo/music/Main.java
@@ -8,7 +8,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Данный класс считывает url адреса странички и регулярные вырожении,
+ * разделяет их(строка представляет собой: адрес \\s регулярное вырожение),
+ * отправляет в ассоциативный массив(ключ - адрес саайта, значение - регулярное вырожение),
+ * после всех переданных ключей и значений в ассоциативный массив,
+ * далбше он передается в класс ParrserHtmlSafeResultInFileTXT,
+ * запуск скачивание музыки.
  *
+ * @author Gavrikov V. 15it18.
  */
 public class Main {
 
@@ -24,30 +31,22 @@ public class Main {
 
             while ((str = inFile.readLine()) != null) {
                 splitStr = str.split(" ");
-//                for (int i = 0; i <splitStr.length ; i++) {
-//                 System.out.println(splitStr[0]);
-//                System.out.println(splitStr[1]);
-//                }
+
                 pathAndURL.put(splitStr[0], splitStr[1]);
+
 
             }
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        String[] strings = pathAndURL.keySet().toArray(new String[0]);
-        for (int i = 0; i <strings.length ; i++) {
-            System.out.println(pathAndURL.get(strings[i]));
-        }
-//        pathAndURL.put("https://zvonko.me/", "\\s*(?<=data-url\\s?=\\s?\")[^>]*\\/*(?=\")");
-//        pathAndURL.put("https://mp3up.me/", "\\s*(?<=audio\\s?src=\")[^>]*\\.mp3");
-        //ParrserHtmlSafeResultInFileTXT.parrseout(pathAndURL);
+        ParrserHtmlSafeResultInFileTXT.parrseout(pathAndURL);
 
 
-     //   MusicDownload music1 = new MusicDownload();
+        MusicDownload download = new MusicDownload();
 
 
-       // music1.start();
+        download.start();
     }
 
 

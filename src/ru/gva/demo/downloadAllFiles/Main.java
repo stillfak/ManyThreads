71657a06bbd8b863dc823196bf97a57d9f170393
vadim <vadim.gavrikov.1998@gmail.com>
2017-12-27 20:34:05 +0300

    Класс Main стал тестовым

--- src/ru/gva/demo/downloadAllFiles/Main.java
+++ src/ru/gva/demo/downloadAllFiles/Main.java
@@ -1,9 +1,6 @@
 package ru.gva.demo.downloadAllFiles;
 
 
-import ru.gva.demo.input.Input;
-
-import java.util.regex.*;
 
 /**
  * В классе реализованно простое консольное управление программой.
@@ -14,25 +11,9 @@ import java.util.regex.*;
 public class Main {
 
     public static void main(String[] args) {
-        String pathOrUrl = Input.inputInConsole("Введите адрес до скачиваемого Файла или путь до файла(.txt) с ссылками");
 
-        Pattern pattern = Pattern.compile(".*\\.txt");
-        Pattern urlPattern = Pattern.compile("^http[s]?:\\/\\/.*$");
-        Matcher matcher = pattern.matcher(pathOrUrl);
-        Matcher matcher1 = urlPattern.matcher(pathOrUrl);
+        Activity.launch(args);
 
-        if (matcher.find()) {
-
-            String extes = Input.inputInConsole("Введите расширение файла");
-
-            new UrlInFiles(matcher.group(), extes).start();
-
-        } else if (matcher1.find()){
-
-            String pathInOS = Input.inputInConsole("Введите путь, куда сохранаять файл, его имя и расширение ");
-
-            new UrlInConsole(matcher1.group(), pathInOS).download();
-        }
 
 
     }
